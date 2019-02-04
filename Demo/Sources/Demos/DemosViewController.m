62f248bbfc367b39af65802594c0f90fd027de4e
Samuel Défago <defagos@gmail.com>
2019-02-04 16:14:20 +0100

    Display history in demo

--- Demo/Sources/Demos/DemosViewController.m
+++ Demo/Sources/Demos/DemosViewController.m
@@ -6,10 +6,15 @@
 
 #import "DemosViewController.h"
 
+#import <libextobjc/libextobjc.h>
+#import <SRGDataProvider/SRGDataProvider.h>
 #import <SRGIdentity/SRGIdentity.h>
 #import <SRGUserData/SRGUserData.h>
 
 @interface DemosViewController ()
+
+@property (nonatomic) NSArray<SRGMedia *> *medias;
+@property (nonatomic, weak) SRGBaseRequest *request;
 
 @end
 
@@ -36,6 +41,10 @@
 {
     [super viewDidLoad];
     
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl = refreshControl;
+    
     [NSNotificationCenter.defaultCenter addObserver:self
                                            selector:@selector(didLogin:)
                                                name:SRGIdentityServiceUserDidLoginNotification
@@ -45,12 +54,46 @@
                                                name:SRGIdentityServiceDidUpdateAccountNotification
                                              object:SRGIdentityService.currentIdentityService];
     
-    [self reloadData];
+    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"MediaCell"];
+    
+    [self updateNavigationBar];
+    [self refresh];
 }
-     
+
+#pragma mark Data
+
+- (void)refresh
+{
+    if (self.request) {
+        return;
+    }
+    
+    NSArray<NSString *> *mediaURNs = [SRGUserData.currentUserData performMainThreadReadTask:^id _Nonnull(NSManagedObjectContext * _Nonnull managedObjectContext) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"%K == NO", @keypath(SRGHistoryEntry.new, discarded)];
+        NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@keypath(SRGHistoryEntry.new, date) ascending:NO];
+        NSArray<SRGHistoryEntry *> *historyEntries = [SRGHistoryEntry historyEntriesMatchingPredicate:predicate sortedWithDescriptors:@[sortDescriptor] inManagedObjectContext:managedObjectContext];
+        return [historyEntries valueForKeyPath:@keypath(SRGHistoryEntry.new, mediaURN)];
+    }];
+
+    SRGBaseRequest *request = [[SRGDataProvider.currentDataProvider mediasWithURNs:mediaURNs completionBlock:^(NSArray<SRGMedia *> * _Nullable medias, SRGPage * _Nonnull page, SRGPage * _Nullable nextPage, NSHTTPURLResponse * _Nullable HTTPResponse, NSError * _Nullable error) {
+        if (self.refreshControl.refreshing) {
+            [self.refreshControl endRefreshing];
+        }
+        
+        if (error) {
+            return;
+        }
+        
+        self.medias = medias;
+        [self.tableView reloadData];
+    }] requestWithPageSize:50];
+    [request resume];
+    self.request = request;
+}
+
 #pragma mark User interface
 
-- (void)reloadData
+- (void)updateNavigationBar
 {
     if (! SRGIdentityService.currentIdentityService.loggedIn) {
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Login", nil)
@@ -66,6 +109,30 @@
     }
 }
 
+#pragma mark UITableViewDataSource protocol
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.medias.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [tableView dequeueReusableCellWithIdentifier:@"MediaCell"];
+}
+
+#pragma mark UITableViewDelegate protocol
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    cell.textLabel.text = self.medias[indexPath.row].title;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 #pragma mark Actions
 
 - (void)login:(id)sender
@@ -78,16 +145,21 @@
     [SRGIdentityService.currentIdentityService logout];
 }
 
+- (void)refresh:(id)sender
+{
+    [self refresh];
+}
+
 #pragma mark Notifications
 
 - (void)didLogin:(NSNotification *)notification
 {
-    [self reloadData];
+    [self updateNavigationBar];
 }
 
 - (void)didUpdateAccount:(NSNotification *)notification
 {
-    [self reloadData];
+    [self updateNavigationBar];
 }
 
 @end
