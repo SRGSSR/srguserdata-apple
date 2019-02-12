6b89f611bdb6beeeb846f36a39b3c12e512dbde9
Samuel Défago <defagos@gmail.com>
2019-02-12 15:14:55 +0100

    Test I/O operations

--- Tests/Sources/DataStoreTestCase.m
+++ Tests/Sources/DataStoreTestCase.m
@@ -46,23 +46,61 @@
 
 - (void)testMainThreadReadTask
 {
-    
+    SRGDataStore *dataStore = [self testDataStoreFromPackage:@"TestData_1"];
+    NSArray<Person *> *persons = [dataStore performMainThreadReadTask:^id _Nullable(NSManagedObjectContext * _Nonnull managedObjectContext) {
+        XCTAssertTrue(NSThread.isMainThread);
+        return [managedObjectContext executeFetchRequest:[Person fetchRequest] error:NULL];
+    }];
+    XCTAssertEqual(persons.count, 1);
+    XCTAssertEqualObjects(persons.firstObject.name, @"Boris");
 }
 
 - (void)testBackgroundReadTask
 {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Read"];
     
+    SRGDataStore *dataStore = [self testDataStoreFromPackage:@"TestData_1"];
+    [dataStore performBackgroundReadTask:^id _Nullable(NSManagedObjectContext * _Nonnull managedObjectContext) {
+        XCTAssertFalse(NSThread.isMainThread);
+        return [managedObjectContext executeFetchRequest:[Person fetchRequest] error:NULL].firstObject;
+    } withPriority:NSOperationQueuePriorityNormal completionBlock:^(id _Nullable result) {
+        XCTAssertFalse(NSThread.isMainThread);
+        XCTAssertTrue([result isKindOfClass:Person.class]);
+        
+        Person *person = result;
+        XCTAssertEqualObjects(person.name, @"Boris");
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:3. handler:nil];
 }
 
 - (void)testBackgroundWriteTask
 {
-    SRGDataStore *dataStore = [self testDataStoreFromPackage:nil];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Write"];
     
+    SRGDataStore *dataStore = [self testDataStoreFromPackage:nil];
     [dataStore performBackgroundWriteTask:^BOOL(NSManagedObjectContext * _Nonnull managedObjectContext) {
+        XCTAssertFalse(NSThread.isMainThread);
         Person *person = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass(Person.class) inManagedObjectContext:managedObjectContext];
-        person.name = @"Boris";
+        person.name = @"James";
         return YES;
-    } withPriority:NSOperationQueuePriorityNormal completionBlock:nil];
+    } withPriority:NSOperationQueuePriorityNormal completionBlock:^(NSError * _Nullable error) {
+        XCTAssertFalse(NSThread.isMainThread);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:3. handler:nil];
+    
+    NSArray<Person *> *persons = [dataStore performMainThreadReadTask:^id _Nullable(NSManagedObjectContext * _Nonnull managedObjectContext) {
+        XCTAssertTrue(NSThread.isMainThread);
+        return [managedObjectContext executeFetchRequest:[Person fetchRequest] error:NULL];
+    }];
+    XCTAssertEqual(persons.count, 1);
+    
+    Person *person = persons.firstObject;
+    XCTAssertEqualObjects(person.name, @"James");
 }
 
 - (void)testTaskOrder
