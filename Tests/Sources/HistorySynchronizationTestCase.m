94edc0255c3a7e65ddc08f43bfa6d5b280b7a57b
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-02-12 18:45:10 +0100

    Start history offline tests

---
+++ Tests/Sources/HistorySynchronizationTestCase.m
@@ -0,0 +1,69 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SRGHistorySynchronizationTestCase : XCTestCase
+
+@end
+
+@implementation SRGHistorySynchronizationTestCase
+
+#pragma mark Tests
+
+- (void)testEmptyHistoryInitialSynchronization
+{
+    
+}
+
+- (void)testHistoryInitialSynchronization
+{
+    
+}
+
+- (void)testHistoryInitialSynchronizationWithExistingLocalEntries
+{
+    
+}
+
+- (void)testSynchronizationWithNewRemoteEntries
+{
+    
+}
+
+- (void)testSynchronizationWithNoNewRemoteEntries
+{
+    
+}
+
+- (void)testSynchronizationWithNewLocalEntries
+{
+    
+}
+
+- (void)testSynchronizationWithNoNewLocalEntries
+{
+    
+}
+
+- (void)testSynchronizationWithNewLocalAndRemoteEntries
+{
+    
+}
+
+- (void)testLogoutDuringSynchronization
+{
+    
+}
+
+- (void)testLogout
+{
+    
+}
+
+// etc.
+
+@end
--- Tests/Sources/HistoryTestCase.m
+++ Tests/Sources/HistoryTestCase.m
@@ -6,64 +6,146 @@
 
 #import <XCTest/XCTest.h>
 
+#import <SRGUserData/SRGUserData.h>
+
 @interface SRGHistoryTestCase : XCTestCase
+
+@property (nonatomic) SRGUserData *userData;
 
 @end
 
 @implementation SRGHistoryTestCase
 
+#pragma mark Setup and tear down
+
+- (void)setUp
+{
+    NSString *libraryDirectory = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject;
+    NSURL *fileURL = [[[NSURL fileURLWithPath:libraryDirectory] URLByAppendingPathComponent:self.name] URLByAppendingPathExtension:@"sqlite"];
+    self.userData = [[SRGUserData alloc] initWithIdentityService:nil historyServiceURL:nil storeFileURL:fileURL];
+}
+
+- (void)tearDown
+{
+    self.userData = nil;
+    
+    NSString *libraryDirectory = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject;
+    NSURL *fileURL = [[[NSURL fileURLWithPath:libraryDirectory] URLByAppendingPathComponent:self.name] URLByAppendingPathExtension:@"sqlite"];
+    [NSFileManager.defaultManager removeItemAtURL:fileURL error:NULL];
+}
+
 #pragma mark Tests
 
-- (void)testEmptyHistoryInitialSynchronization
+- (void)testEmptyHistoryInitialization
 {
+    XCTAssertNotNil(self.userData.history);
     
+    NSArray<SRGHistoryEntry *> *historyEntries = [self.userData.history historyEntriesMatchingPredicate:nil sortedWithDescriptors:nil];
+    
+    XCTAssertEqual(historyEntries.count, 0);
 }
 
-- (void)testHistoryInitialSynchronization
+- (void)testHistorySaveHistoryEntry
 {
+    NSString *uid = @"1234";
+    CMTime time = CMTimeMakeWithSeconds(10, NSEC_PER_SEC);
+    ;
+    NSString *deviceUid = @"Test device";
     
+    [self expectationForNotification:SRGHistoryDidChangeNotification object:self.userData.history handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertTrue(NSThread.isMainThread);
+        NSArray<NSString *> *uids = notification.userInfo[SRGHistoryUidsKey];
+        return [uids containsObject:uid];
+    }];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"History entry saved"];
+    
+    [self.userData.history saveHistoryEntryForUid:uid withLastPlaybackTime:time deviceUid:deviceUid completionBlock:^(NSError * _Nonnull error) {
+        XCTAssertNil(error);
+        [expectation fulfill];
+        
+    }];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
-- (void)testHistoryInitialSynchronizationWithExistingLocalEntries
+- (void)testHistoryEntryWithUid
 {
+    NSString *uid = @"1234";
+    CMTime time = CMTimeMakeWithSeconds(10, NSEC_PER_SEC);
+    ;
+    NSString *deviceUid = @"Test device";
     
+    XCTestExpectation *expectation = [self expectationWithDescription:@"History entry saved"];
+    
+    [self.userData.history saveHistoryEntryForUid:uid withLastPlaybackTime:time deviceUid:deviceUid completionBlock:^(NSError * _Nonnull error) {
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
+    
+    SRGHistoryEntry *historyEntry = [self.userData.history historyEntryWithUid:uid];
+    
+    XCTAssertEqualObjects(historyEntry.uid, uid);
+    XCTAssertTrue(CMTIME_COMPARE_INLINE(historyEntry.lastPlaybackTime, ==, time));
+    XCTAssertEqualObjects(historyEntry.deviceUid, deviceUid);
 }
 
-- (void)testSynchronizationWithNewRemoteEntries
+- (void)testHistoryEntryWithUidAsynchronously
 {
+    NSString *uid = @"1234";
+    CMTime time = CMTimeMakeWithSeconds(10, NSEC_PER_SEC);
+    ;
+    NSString *deviceUid = @"Test device";
     
+    XCTestExpectation *expectation1 = [self expectationWithDescription:@"History entry saved"];
+    
+    [self.userData.history saveHistoryEntryForUid:uid withLastPlaybackTime:time deviceUid:deviceUid completionBlock:^(NSError * _Nonnull error) {
+        XCTAssertNil(error);
+        [expectation1 fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
+    
+    XCTestExpectation *expectation2 = [self expectationWithDescription:@"History entry fetched"];
+    
+    [self.userData.history historyEntryWithUid:uid completionBlock:^(SRGHistoryEntry * _Nullable historyEntry) {
+        XCTAssertFalse(NSThread.isMainThread);
+        XCTAssertEqualObjects(historyEntry.uid, uid);
+        XCTAssertTrue(CMTIME_COMPARE_INLINE(historyEntry.lastPlaybackTime, ==, time));
+        XCTAssertEqualObjects(historyEntry.deviceUid, deviceUid);
+        [expectation2 fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
-- (void)testSynchronizationWithNoNewRemoteEntries
+- (void)testHistoryEntriesMatchingEmptyPredicateEmptySortedhDescriptor
 {
+    NSString *uid = @"1234";
+    CMTime time = CMTimeMakeWithSeconds(10, NSEC_PER_SEC);
+    ;
+    NSString *deviceUid = @"Test device";
     
-}
-
-- (void)testSynchronizationWithNewLocalEntries
-{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"History entry saved"];
     
-}
-
-- (void)testSynchronizationWithNoNewLocalEntries
-{
+    [self.userData.history saveHistoryEntryForUid:uid withLastPlaybackTime:time deviceUid:deviceUid completionBlock:^(NSError * _Nonnull error) {
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
     
-}
-
-- (void)testSynchronizationWithNewLocalAndRemoteEntries
-{
+    [self waitForExpectationsWithTimeout:30. handler:nil];
     
-}
-
-- (void)testLogoutDuringSynchronization
-{
+    NSArray<SRGHistoryEntry *> *historyEntries = [self.userData.history historyEntriesMatchingPredicate:nil sortedWithDescriptors:nil];
     
-}
-
-- (void)testLogout
-{
+    XCTAssertEqual(historyEntries.count, 1);
     
+    SRGHistoryEntry *historyEntry = historyEntries.firstObject;
+    
+    XCTAssertEqualObjects(historyEntry.uid, uid);
+    XCTAssertTrue(CMTIME_COMPARE_INLINE(historyEntry.lastPlaybackTime, ==, time));
+    XCTAssertEqualObjects(historyEntry.deviceUid, deviceUid);
 }
-
-// etc.
 
 @end
