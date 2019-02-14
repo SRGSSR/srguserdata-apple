36bd0d867edcc7cd48898724edb849cecd81caa4
Samuel Défago <defagos@gmail.com>
2019-02-14 11:35:54 +0100

    Ensure predicates can be made around playback positions

--- Framework/Sources/Model/SRGHistoryEntry.h
+++ Framework/Sources/Model/SRGHistoryEntry.h
@@ -18,14 +18,28 @@ NS_ASSUME_NONNULL_BEGIN
 @interface SRGHistoryEntry : SRGUserObject
 
 /**
- *  The playback position which the item was played at.
+ *  The playback position which the item was played at (in seconds).
  */
-@property (nonatomic, readonly) CMTime lastPlaybackTime;
+@property (nonatomic, readonly) double lastPlaybackPosition;
 
 /**
  *  An identifier for the device which updated the entry.
  */
 @property (nonatomic, readonly, copy, nullable) NSString *deviceUid;
+
+@end
+
+/**
+ *  Transient fields.
+ *
+ *  @discussion These fields can be used for building filter predicates.
+ */
+@interface SRGHistoryEntry (Transient)
+
+/**
+ *  The playback position which the item was played at (as a `CMTime`).
+ */
+@property (nonatomic, readonly) CMTime lastPlaybackTime;
 
 @end
 
