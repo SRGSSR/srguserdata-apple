6ea51a127f1009c5800c4656b7b50e3a7b224860
Samuel Défago <defagos@gmail.com>
2019-02-15 08:57:43 +0100

    Display alert when a user has been unauthorized

--- Demo/Sources/Application/AppDelegate.m
+++ Demo/Sources/Application/AppDelegate.m
@@ -32,9 +32,27 @@
     
     SRGDataProvider.currentDataProvider = [[SRGDataProvider alloc] initWithServiceURL:SRGIntegrationLayerProductionServiceURL()];
     
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(userDidLogout:)
+                                               name:SRGIdentityServiceUserDidLogoutNotification
+                                             object:nil];
+    
     DemosViewController *demosViewController = [[DemosViewController alloc] init];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:demosViewController];
     return YES;
+}
+
+#pragma mark Notifications
+
+- (void)userDidLogout:(NSNotification *)notification
+{
+    if ([notification.userInfo[SRGIdentityServiceUnauthorizedKey] boolValue]) {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Information", nil)
+                                                                                 message:NSLocalizedString(@"You have been logged out. Please login again to synchronize your data.", nil)
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Dismiss", nil) style:UIAlertActionStyleDefault handler:nil]];
+        [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
+    }
 }
 
 @end
