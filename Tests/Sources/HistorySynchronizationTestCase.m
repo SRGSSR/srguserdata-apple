11411b5b13bd72e32e2a3a026676513d878e9ddd
Samuel Défago <defagos@gmail.com>
2019-02-22 10:12:05 +0100

    Prepare for synchronization tests

--- Tests/Sources/HistorySynchronizationTestCase.m
+++ Tests/Sources/HistorySynchronizationTestCase.m
@@ -4,19 +4,68 @@
 //  License information is available from the LICENSE file.
 //
 
-#import <XCTest/XCTest.h>
+#import "UserDataBaseTestCase.h"
 
-@interface SRGHistorySynchronizationTestCase : XCTestCase
+#import <SRGIdentity/SRGIdentity.h>
+
+@interface SRGIdentityService (Private)
+
+- (BOOL)handleCallbackURL:(NSURL *)callbackURL;
+
+@property (nonatomic, readonly, copy) NSString *identifier;
+
+@end
+
+static NSURL *TestWebserviceURL(void)
+{
+    return [NSURL URLWithString:@"https://api.srgssr.local"];
+}
+
+static NSURL *TestWebsiteURL(void)
+{
+    return [NSURL URLWithString:@"https://www.srgssr.local"];
+}
+
+static NSURL *TestLoginCallbackURL(SRGIdentityService *identityService, NSString *token)
+{
+    NSString *URLString = [NSString stringWithFormat:@"srguserdata-tests://%@?identity_service=%@&token=%@", TestWebserviceURL().host, identityService.identifier, token];
+    return [NSURL URLWithString:URLString];
+}
+
+@interface SRGHistorySynchronizationTestCase : UserDataBaseTestCase
+
+@property (nonatomic) SRGIdentityService *identityService;
+@property (nonatomic) SRGUserData *userData;
 
 @end
 
 @implementation SRGHistorySynchronizationTestCase
 
+#pragma mark Setup and teardown
+
+- (void)setUp
+{
+    self.identityService = [[SRGIdentityService alloc] initWithWebserviceURL:TestWebserviceURL() websiteURL:TestWebsiteURL()];
+    
+    NSURL *fileURL = [[[NSURL fileURLWithPath:NSTemporaryDirectory()] URLByAppendingPathComponent:NSUUID.UUID.UUIDString] URLByAppendingPathExtension:@"sqlite"];
+    self.userData = [[SRGUserData alloc] initWithStoreFileURL:fileURL historyServiceURL:nil identityService:self.identityService];
+    
+    [self expectationForNotification:SRGIdentityServiceUserDidLoginNotification object:self.identityService handler:^BOOL(NSNotification * _Nonnull notification) {
+        return YES;
+    }];
+    
+    // playsrgtests+userdata1@gmail.com
+    BOOL hasHandledCallbackURL = [self.identityService handleCallbackURL:TestLoginCallbackURL(self.identityService, @"s%3AywJ2a0JqEvMOe-rzgZbYqvSeBSg0bb9P.2qChe6iS9BEEf%2FXPTjJ8Nosw2r1JACPRgxyFaXSrz6U")];
+    XCTAssertTrue(hasHandledCallbackURL);
+    
+    [self waitForExpectationsWithTimeout:5. handler:nil];
+}
+
 #pragma mark Tests
 
 - (void)testEmptyHistoryInitialSynchronization
 {
-    // TODO: Check that sync date is not nil afterwards
+    
 }
 
 - (void)testHistoryInitialSynchronization
