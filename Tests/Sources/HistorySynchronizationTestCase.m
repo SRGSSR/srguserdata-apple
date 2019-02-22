909b1ce5663190fea937914f8e47878e09aa0f6a
Samuel Défago <defagos@gmail.com>
2019-02-22 10:35:20 +0100

    Cleanup local history before tests

--- Tests/Sources/HistorySynchronizationTestCase.m
+++ Tests/Sources/HistorySynchronizationTestCase.m
@@ -16,6 +16,12 @@
 
 @end
 
+@interface SRGHistory (Private)
+
+- (void)synchronize;
+
+@end
+
 static NSURL *TestWebserviceURL(void)
 {
     return [NSURL URLWithString:@"https://api.srgssr.local"];
@@ -46,14 +52,31 @@ static NSURL *TestLoginCallbackURL(SRGIdentityService *identityService, NSString
 - (void)setUp
 {
     self.identityService = [[SRGIdentityService alloc] initWithWebserviceURL:TestWebserviceURL() websiteURL:TestWebsiteURL()];
-    
+ 
+    // We could mock history services to implement true unit tests, but to be able to catch their possible issues (!),
+    // we rather want integration tests. We therefore need to use a test user, simulating login by injecting its
+    // session token into the identity service.
     NSURL *fileURL = [[[NSURL fileURLWithPath:NSTemporaryDirectory()] URLByAppendingPathComponent:NSUUID.UUID.UUIDString] URLByAppendingPathExtension:@"sqlite"];
-    self.userData = [[SRGUserData alloc] initWithStoreFileURL:fileURL historyServiceURL:nil identityService:self.identityService];
+    self.userData = [[SRGUserData alloc] initWithStoreFileURL:fileURL
+                                            historyServiceURL:[NSURL URLWithString:@"https://profil.rts.ch/api/history"]
+                                              identityService:self.identityService];
     
-    // playsrgtests+userdata1@gmail.com
+    // Logout with playsrgtests+userdata1@gmail.com
     BOOL hasHandledCallbackURL = [self.identityService handleCallbackURL:TestLoginCallbackURL(self.identityService, @"s%3AywJ2a0JqEvMOe-rzgZbYqvSeBSg0bb9P.2qChe6iS9BEEf%2FXPTjJ8Nosw2r1JACPRgxyFaXSrz6U")];
     XCTAssertTrue(hasHandledCallbackURL);
     XCTAssertNotNil(self.identityService.sessionToken);
+    
+    [self expectationForNotification:SRGHistoryDidChangeNotification object:self.userData.history handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertEqual([notification.userInfo[SRGHistoryUidsKey] count], 0);
+        return YES;
+    }];
+    
+    // Reset user history
+    [self.userData.history discardHistoryEntriesWithUids:nil completionBlock:^(NSError * _Nonnull error) {
+        [self.userData.history synchronize];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5. handler:nil];
 }
 
 #pragma mark Tests
