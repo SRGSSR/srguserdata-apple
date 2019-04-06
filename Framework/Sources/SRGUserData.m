91fff4bb1d1e069e8ab60f0630baa391344cbb06
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-04-06 13:07:11 +0200

    Add core data migration from v6 to v7

--- Framework/Sources/SRGUserData.m
+++ Framework/Sources/SRGUserData.m
@@ -17,7 +17,7 @@
 
 #import <libextobjc/libextobjc.h>
 
-static NSUInteger s_currentPersistentStoreVersion = 6;
+static NSUInteger s_currentPersistentStoreVersion = 7;
 
 typedef NSString * SRGUserDataServiceType NS_TYPED_ENUM;
 static SRGUserDataServiceType const SRGUserDataServiceTypeHistory = @"History";
