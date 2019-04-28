8d6de101ac950c574d06985edcbdc7511d60a7c4
Samuel Défago <defagos@gmail.com>
2019-04-28 16:00:53 +0200

    Test system playlist synchronization

--- Tests/Sources/PlaylistsSynchronizationTestCase.m
+++ Tests/Sources/PlaylistsSynchronizationTestCase.m
@@ -23,7 +23,7 @@
 
 #pragma mark Tests
 
-- (void)testEmptySynchronization
+- (void)testSystemPlaylistSynchronization
 {
     [self setupForAvailableService];
     [self loginAndWaitForInitalSynchronization];
@@ -37,22 +37,22 @@
         return YES;
     }];
     
-    [self expectationForElapsedTimeInterval:5. withHandler:nil];
-    id changeObserver = [NSNotificationCenter.defaultCenter addObserverForName:SRGPlaylistsDidChangeNotification object:self.userData.playlists queue:nil usingBlock:^(NSNotification * _Nonnull notification) {
-        XCTFail(@"No change notification is expected. Playlists are empty and still must be");
+    [self expectationForNotification:SRGPlaylistsDidChangeNotification object:self.userData.playlists handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertEqualObjects(notification.userInfo[SRGPlaylistsPreviousUidsKey], @[@"watch_later"]);
+        XCTAssertEqualObjects(notification.userInfo[SRGPlaylistsChangedUidsKey], @[@"watch_later"]);
+        XCTAssertEqualObjects(notification.userInfo[SRGPlaylistsUidsKey], @[@"watch_later"]);
+        return YES;
     }];
     
     [self.userData synchronize];
     
-    [self waitForExpectationsWithTimeout:10. handler:^(NSError * _Nullable error) {
-        [NSNotificationCenter.defaultCenter removeObserver:changeObserver];
-    }];
+    [self waitForExpectationsWithTimeout:10. handler:nil];
     
     XCTestExpectation *expectation = [self expectationWithDescription:@"Playlist request"];
     
     [[SRGPlaylistsRequest playlistsFromServiceURL:TestPlaylistsServiceURL() forSessionToken:self.identityService.sessionToken withSession:NSURLSession.sharedSession completionBlock:^(NSArray<NSDictionary *> * _Nullable playlistDictionaries, NSHTTPURLResponse * _Nullable HTTPResponse, NSError * _Nullable error) {
         XCTAssertNil(error);
-        XCTAssertEqual(playlistDictionaries.count, 0);
+        XCTAssertEqual(playlistDictionaries.count, 1);
         [expectation fulfill];
     }] resume];
     
