fefeab031875be17429b47f7c938ff2f78251873
Samuel Défago <defagos@gmail.com>
2019-05-13 11:40:27 +0200

    Prepare preferences public API

--- Framework/Sources/Preferences/SRGPreferences.h
+++ Framework/Sources/Preferences/SRGPreferences.h
@@ -10,6 +10,26 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SRGPreferences : SRGUserDataService
 
+- (NSDictionary *)preferencesInDomain:(NSString *)domain;
+
+- (void)setString:(NSString *)string forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (void)setNumber:(NSNumber *)number forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (void)setArray:(NSArray *)array forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+
+- (void)setBool:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (void)setInteger:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (void)setFloat:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (void)setDouble:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+
+- (NSString *)stringForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (NSNumber *)numberForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (NSArray *)arrayForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+
+- (BOOL)boolForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (NSInteger)integerForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (float)floatForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+- (double)doubleForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Framework/Sources/Preferences/SRGPreferences.m
+++ Framework/Sources/Preferences/SRGPreferences.m
@@ -8,4 +8,81 @@
 
 @implementation SRGPreferences
 
+#pragma mark Getters and setters
+
+- (NSDictionary *)preferencesInDomain:(NSString *)domain
+{
+    return nil;
+}
+
+- (void)setString:(NSString *)string forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setNumber:(NSNumber *)number forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setArray:(NSArray *)array forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setBool:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setInteger:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setFloat:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (void)setDouble:(BOOL)value forKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    
+}
+
+- (NSString *)stringForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return nil;
+}
+
+- (NSNumber *)numberForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return nil;
+}
+
+- (NSArray *)arrayForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return nil;
+}
+
+- (BOOL)boolForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return NO;
+}
+
+- (NSInteger)integerForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return 0;
+}
+
+- (float)floatForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return 0.f;
+}
+
+- (double)doubleForKeyPath:(NSString *)keyPath inDomain:(NSString *)domain
+{
+    return 0.;
+}
+
 @end
