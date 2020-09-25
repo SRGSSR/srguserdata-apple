9219a26b0a2a3f9a7b7c9aa7071b7f0e261e0488
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-09-25 18:57:30 +0200

    Update dependencies with SPM on main branches

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "SRGIdentity", url: "https://github.com/SRGSSR/srgidentity-apple.git", .branch("feature/spm-support"))
+        .package(name: "SRGIdentity", url: "https://github.com/SRGSSR/srgidentity-apple.git", .branch("develop"))
     ],
     targets: [
         .target(
