aa583c0f474d746c2770a3ec1f7772d23491eb67
Samuel Défago <defagos@gmail.com>
2022-07-08 09:12:45 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "SRGIdentity", url: "https://github.com/SRGSSR/srgidentity-apple.git", .upToNextMinor(from: "3.2.1"))
+        .package(name: "SRGIdentity", url: "https://github.com/SRGSSR/srgidentity-apple.git", .branch("feature/ios16-tvos16-xcode14"))
     ],
     targets: [
         .target(
